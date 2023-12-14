1039dfd1fb0b0359febec19eb63c9f6027ba9815
BikromRoy1 <bikrom@peoplentech.com.bd>
2023-12-14 18:20:30 +0600

    Testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -190,6 +190,24 @@
   });
 
   /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+  });
+
+  /**
    * Skills animation
    */
   let skilsContent = select('.skills-content');
